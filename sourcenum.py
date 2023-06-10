6c8dfd59fe48408137c02f489feca141023ab2a2
bouyacoding <bjaroensawas@gmail.com>
2023-06-10 16:11:52 +0700

    sortsystem

---
+++ sourcenum.py
@@ -0,0 +1,11 @@
+numbers = [20,10,34,35,13,12,65,76,23,11]
+
+print(numbers)
+
+numbers.sort()
+
+print(numbers)
+
+numbers.sort(reverse = True)
+
+print(numbers)
---
+++ sourcestr.py
@@ -0,0 +1,11 @@
+names = ["bouya","vorachet","jeab","nine"]
+
+print(names)
+
+names.sort()
+
+print(names)
+
+names.sort(reverse = True)
+
+print(names)
